3f7d313edbc1498e22a22d3d5fd5637345ff226d
Jesu <97321002+237mrt@users.noreply.github.com>
2023-05-05 22:09:45 +0300

    Delete src directory

--- src/commands/prefix/ping.js
+++
@@ -1,9 +0,0 @@
-export const data ={
-    name:'oy',
-    desc:'Ping komutu',
-    cooldown:70, // komutun kaç saniyede bir kullanılacağını belirtir
-    slash:false, // slash komutu olup olmadığını belirtir
-    execute(message, args){
-        message.reply('Pong')
-    }
-}
